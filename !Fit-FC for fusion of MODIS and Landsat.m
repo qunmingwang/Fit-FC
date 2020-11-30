35cc2ae56b49244e0afc7ed62c0825278c447e9e
qunmingwang <71642787+qunmingwang@users.noreply.github.com>
2020-11-30 14:55:49 +0800

    Update !Fit-FC for fusion of MODIS and Landsat.m

--- !Fit-FC for fusion of MODIS and Landsat.m
+++ !Fit-FC for fusion of MODIS and Landsat.m
@@ -1,3 +1,7 @@
+%%%%This is the code for Fit-FC produced by Dr Qunming Wang; Email: wqm11111@126.com
+%%%%Copyright belongs to Qunming Wang
+%%%%When using the code, please cite the fowllowing papers
+%%%%Q. Wang, P. M. Atkinson. Spatio-temporal fusion for daily Sentinel-2 images. Remote Sensing of Environment, 2018, 204: 31–42.
 clear all;
 load MODIS_t1;
 load MODIS_t2;
